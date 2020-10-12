3f07465fdd37dbe16d1b0270a250a8a75b6eb2e9
heitao <heitao5200@163.com>
2020-10-12 22:20:39 +0800

    add hello.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding:utf-8
+"""
+Project Name: 
+File Name : hello.py
+Author  : heitao
+Contect : heitao5200@163.com
+Time    : 2020/10/12 下午10:18
+Desc:
+"""
+
+print(666)
